210018b05e08a4c7c54d566b374800d243352a09
Steve Kuo <stevekuo@stevekuo.local>
2017-01-27 11:38:14 -0800

    Rename HelloWorld to JavaHelloWorld

--- src/main/java/com/coding/lambda/JavaHelloWorld.java
+++ src/main/java/com/coding/lambda/JavaHelloWorld.java
@@ -7,11 +7,15 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
  * Lambda function that simply prints "Hello World" if the input String is not
  * provided, otherwise, print "Hello " with the provided input String.
  */
-public class HelloWorld implements RequestHandler<String, String> {
+public class JavaHelloWorld implements RequestHandler<String, String> {
 	@Override
 	public String handleRequest(String input, Context context) {
+		/*
 		String output = "Hello " + ((input != null && !input.isEmpty()) ? input : "World");
 		context.getLogger().log(output);
 		return output;
+		*/
+		context.getLogger().log(input);
+		return input;		
 	}
 }
