d3a727b3b91d4977c4bbc483928e00002a5fc54e
Steve Kuo <kuosenhao@gmail.com>
2017-01-27 17:01:58 -0800

    Match FacadeRequest up with bodyMappingTemplate

--- src/main/java/com/coding/lambda/FacadeRequest.java
+++ src/main/java/com/coding/lambda/FacadeRequest.java
@@ -3,32 +3,34 @@ package com.coding.lambda;
 import java.util.Map;
 
 public class FacadeRequest {
-	private Map<String,String> pathParams;
-	private Map<String,String> queryParams;
 	private Map<String,String> body;
-
-	public Map<String, String> getPathParams() {
-		return pathParams;
-	}
-
-	public void setPathParams(Map<String, String> pathParams) {
-		this.pathParams = pathParams;
-	}
-
-	public Map<String, String> getQueryParams() {
-		return queryParams;
-	}
-
-	public void setQueryParams(Map<String, String> queryParams) {
-		this.queryParams = queryParams;
-	}
-
+	private Map<String,Map<String,String>> params;
+	private Map<String,String> stage;
+	private Map<String,String> context;
+	
 	public Map<String, String> getBody() {
 		return body;
 	}
-
 	public void setBody(Map<String, String> body) {
 		this.body = body;
+	}
+	public Map<String, Map<String, String>> getParams() {
+		return params;
+	}
+	public void setParams(Map<String, Map<String, String>> params) {
+		this.params = params;
+	}
+	public Map<String, String> getStage() {
+		return stage;
+	}
+	public void setStage(Map<String, String> stage) {
+		this.stage = stage;
+	}
+	public Map<String, String> getContext() {
+		return context;
+	}
+	public void setContext(Map<String, String> context) {
+		this.context = context;
 	}
 	
 }
--- src/main/java/com/coding/lambda/HelloWorldFacade.java
+++ src/main/java/com/coding/lambda/HelloWorldFacade.java
@@ -3,37 +3,96 @@ package com.coding.lambda;
 import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class HelloWorldFacade {
 
 	/* The expected json
 {
-	"pathParams":
-	{
-		"name": "bright"
-	}
-	"queryParams":
-	{
-		"breed": "curly coded retriever"
-	}
-	"body": 
-  	{
-		"firstName": "DogVacay",
-		"lastName": "Facade",
-  	}
+  "body": {
+	"firstName": "DogVacay",
+	"lastName": "Facade",
+  }
+  "parameters" : {
+     "path" : {    
+       "path_name" : "path_value", 
+       ...
+     }
+     "header" : {  
+       "header_name" : "header_value",
+       ...
+     }
+     'querystring" : {
+       "querystring_name" : "querystring_value",
+       ...
+     }
+   },
+   "stage" : {
+     
+   },
+  "context" : {
+    "account-id" : "$context.identity.accountId",
+    "api-id" : "$context.apiId",
+    "api-key" : "$context.identity.apiKey",
+    "authorizer-principal-id" : "$context.authorizer.principalId",
+    "caller" : "$context.identity.caller",
+    "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+    "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+    "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+    "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+    "http-method" : "$context.httpMethod",
+    "stage" : "$context.stage",
+    "source-ip" : "$context.identity.sourceIp",
+    "user" : "$context.identity.user",
+    "user-agent" : "$context.identity.userAgent",
+    "user-arn" : "$context.identity.userArn",
+    "request-id" : "$context.requestId",
+    "resource-id" : "$context.resourceId",
+    "resource-path" : "$context.resourcePath"
+  }
 }
 	 */
 	public Response handleRequest(FacadeRequest request, Context context) {
-		Map<String,String> body = request.getBody();
-		Map<String,String> queryParams = request.getQueryParams();
-		Map<String,String> pathParams = request.getPathParams();
-		context.getLogger().log(body.get("firstName") + " " + body.get("lastName") + ", name=" + pathParams.get("name") +
-				"breed" + queryParams.get("breed"));
+		LambdaLogger logger = context.getLogger();
+		// body
+		Map<String, String> body = request.getBody();
+		logReqMap("body", body, logger);
+		// parameters
+		Map<String,Map<String,String>> parameters = request.getParams();
+		logMapOfReqMap("parameters", parameters, logger);
+		// stage
+		Map<String,String> stage = request.getStage();
+		logReqMap("stage", stage, logger);
+		// context
+		Map<String,String> ctx = request.getContext();
+		logReqMap("context", ctx, logger);
+		
+		// construct response
 		String greetingStr = String.format("Body %s %s. queryParams[name]=%s, pathParams[breed]=%s", 
 				body.get("firstName"), body.get("lastName"),
-				pathParams.get("name"),
-				queryParams.get("breed"));
+				parameters.get("path").get("name"),
+				parameters.get("querystring").get("querystring_breed"));
 		return new Response(greetingStr);		
+	}
+
+	private void logMapOfReqMap(String attr, Map<String,Map<String,String>> mapOfReqMap, LambdaLogger logger) {
+		// error check on null
+		if (mapOfReqMap == null)
+			return;
+		for (Map.Entry<String, Map<String,String>> entry: mapOfReqMap.entrySet()) {
+			logReqMap(attr + " " + entry.getKey(), entry.getValue(), logger);
+		}
+	}
+
+	private void logReqMap(String attr, Map<String,String> reqMap, LambdaLogger logger) {
+		// error check on null
+		if (reqMap == null)
+			return;
+		
+		StringBuilder sb = new StringBuilder("[" + attr + "]   ");
+		for (Map.Entry<String, String> e : reqMap.entrySet())
+			sb.append(e.getKey()+":"+e.getValue() + ", ");
+		logger.log(sb.toString());
 	}
 
 }
